ef59c1719ed9a67937d15a898b0f616b0df5b54c
Chris Fung <aergonaut@gmail.com>
2014-02-20 23:50:35 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Examwriter::Application.config.secret_key_base = 'f5ce66ec1af650bf0adea53e95fff1314410eeb0277c0b3576be5a1de83662b0771cc12697a9c4da3fba52f89aa174ae3736c745e1612a582e2d279d1785120d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Examwriter::Application.config.session_store :cookie_store, key: '_examwriter_session'
