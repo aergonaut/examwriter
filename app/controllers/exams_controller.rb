c743e6adfe46e4672389c9e48f62e9d95d416b43
Chris Fung <aergonaut@gmail.com>
2014-02-21 00:28:11 -0800

    crud for exams

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,2 +1,41 @@
 class ExamsController < ApplicationController
+  def index
+    @exams = Exam.all
+  end
+
+  def new
+    @exam = Exam.new
+  end
+
+  def create
+    @exam = Exam.new(exam_params)
+    if @exam.save
+      flash[:success] = "Exam create successfully"
+      redirect_to exams_path
+    else
+      flash[:danger] = "Please fix the errors below"
+      render :new
+    end
+  end
+
+  def edit
+    @exam = Exam.find(params[:id])
+  end
+
+  def update
+    @exam = Exam.find(params[:id])
+    if @exam.update(exam_params)
+      flash[:success] = "Exam updated"
+      redirect_to exams_path
+    else
+      flash[:danger] = "Please fix the errors below"
+      render :edit
+    end
+  end
+
+  private
+
+  def exam_params
+    params.require(:exam).permit(:title)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Examwriter::Application.routes.draw do
 
   resources :exams
 
+  root "exams#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
